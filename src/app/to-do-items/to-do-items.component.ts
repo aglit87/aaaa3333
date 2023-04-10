0be3b0733d068750b4b90707c12fc61bd71208e5
Andrey Litvinov <112853536+aglit87@users.noreply.github.com>
2023-04-10 18:33:06 +0300

    Add files via upload

---
+++ src/app/to-do-items/to-do-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-items',
+  templateUrl: './to-do-items.component.html',
+  styleUrls: ['./to-do-items.component.css']
+})
+export class ToDoItemsComponent {
+  title = 'Полить цветы';
+  price = 4500;
+}
